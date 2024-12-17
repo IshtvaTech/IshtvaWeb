fa2f5910afdef9e5094897ea3684674a56816246
IshtvaTech <techdeveloperishtva@gmail.com>
2024-12-17 18:46:45 +0530

    Update layout.tsx
    updated the name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from '@/components/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Software Consulting Services',
+  title: 'Ishtva Technologies',
   description: 'Professional Software Consulting and Development Services',
 }
 
